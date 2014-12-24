1515bb6eccf02c9be3097d29b2cad8e4105d98d4
Ezra <ezrazigmond@gmail.com>
2014-12-24 10:29:37 -0500

    Fixed mobile

--- js/Boot.js
+++ js/Boot.js
@@ -29,9 +29,12 @@ TileGame.Boot.prototype = {
         {
             //  Same goes for mobile settings.
             //  In this case we're saying "scale the game, no lower than 480x260 and no higher than 1024x768"
-            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-            this.scale.setMinMax(480, 260, 1024, 768);
-            this.scale.forceLandscape = true;
+            this.game.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
+            this.game.scale.setScreenSize();
+            // this.game.scale.refresh();
+            // this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            // this.scale.setMinMax(480, 260, 1024, 768);
+            // this.scale.forceLandscape = true;
             this.scale.pageAlignHorizontally = true;
         }
 
