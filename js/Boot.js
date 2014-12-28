f70b49e397d8df4c7b782687f7d4f11f0e9a941f
Ezra <ezrazigmond@gmail.com>
2014-12-28 15:24:25 -0500

    Trying new scale settings

--- js/Boot.js
+++ js/Boot.js
@@ -26,16 +26,20 @@ TileGame.Boot.prototype = {
         else
         {
             this.scale.pageAlignHorizontally = true;
+            game.stage.scale.startFullScreen();
+            game.stage.scaleMode = Phaser.StageScaleMode.SHOW_ALL; //resize your window to see the stage resize too
+            game.stage.scale.setShowAll();
+            game.stage.scale.refresh();
             // this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
             // this.game.scale.setScreenSize();
             // this.game.scale.refresh();
             //  Same goes for mobile settings.
             //  In this case we're saying "scale the game, no lower than 480x260 and no higher than 1024x768"
-            this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-            this.game.scale.setScreenSize();
-            this.game.scale.refresh();
-            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-            this.scale.setMinMax(480, 260, 1024, 768);
+            // this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            // this.game.scale.setScreenSize();
+            // this.game.scale.refresh();
+            // this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            // this.scale.setMinMax(480, 260, 1024, 768);
             // this.scale.forceLandscape = true;
             // this.scale.pageAlignHorizontally = true;
         }
